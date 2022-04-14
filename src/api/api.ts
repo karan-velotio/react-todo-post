3b66cab215603a3b502ffeaa370dab11249c70b1
Karan Panwar <karan.panwar@velotio.com>
2022-04-14 18:09:53 +0530

    url http replace with https

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://jsonplaceholder.typicode.com/`,
+  baseURL: `https://jsonplaceholder.typicode.com/`,
   headers: { 'Content-Type': 'application/json' }
 });
