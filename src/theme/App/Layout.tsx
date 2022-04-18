69f90e1c757c4e1febe75aece5552d16d663fa70
Karan Panwar <karan.panwar@velotio.com>
2022-04-18 11:51:36 +0530

    Theme provider added at top level

--- src/theme/App/Layout.tsx
+++ src/theme/App/Layout.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Header from "src/components/Header";
 import { Outlet } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const outerTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#E50914",
+      contrastText: "#F5F5F1",
+    },
+  },
+  typography: {
+    fontFamily: "Noto Sans, sans-serif",
+  },
+});
 
 const Layout = () => {
-  return (<>
-    <Header />
-    <Outlet />
-  </>);
+  return (
+    <ThemeProvider theme={outerTheme}>
+      <Header />
+      <Outlet />
+    </ThemeProvider>
+  );
 };
 
 export default Layout;
